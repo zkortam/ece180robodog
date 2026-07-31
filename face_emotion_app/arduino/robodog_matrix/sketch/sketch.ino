db6966ca2d7da090928d409ac4ee8813034afb98
zkortam <zakaria.kortam@gmail.com>
2026-07-31 12:15:18 -0700

    make matrix status bridge callback serializable

--- face_emotion_app/arduino/robodog_matrix/sketch/sketch.ino
+++ face_emotion_app/arduino/robodog_matrix/sketch/sketch.ino
@@ -155,7 +155,10 @@ void draw_state() {
   matrix.draw(frame);
 }
 
-bool set_robodog_status(String value) {
+// Keep this callback void.  The UNO Q router encodes bool returns as int8,
+// which the current router CLI cannot serialize and which makes every status
+// update panic after the matrix has already stopped receiving commands.
+void set_robodog_status(String value) {
   RobotState requested = STATE_ERROR;
   if (value == "off") requested = STATE_OFF;
   else if (value == "starting") requested = STATE_STARTING;
@@ -173,7 +176,6 @@ bool set_robodog_status(String value) {
     next_frame_at = 0;
   }
   k_mutex_unlock(&display_mutex);
-  return true;
 }
 
 void setup() {
